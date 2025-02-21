34a8f3e0bd268bf9b4f80ab0072874b31cc2c34f
13750296 <yanw6588@gmail.com>
2025-02-21 12:09:51 +0800

    weed01

---
+++ week01-1.cpp
@@ -0,0 +1,10 @@
+///week01-1.cpp要自己打好全部檔名 .cpp
+///在瘋狂程設(練習模式)寫好的程式碼，全選，Ctrl-c
+///CodeBlocks File-New-Empty 空白檔案，Ctrl-s存檔
+
+#include <stdio.h> ///
+{
+   int a, b;
+   scanf("%d%d",&a,&b);
+   printf("%d",a+b);
+}
